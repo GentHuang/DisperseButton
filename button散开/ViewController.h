2a5bd39b76eac75cf7dd2c314d1ec46a6f22ca0c
huangzijun316@163.com <huangzijun316@163.com>
2016-07-16 17:38:31 +0800

    first commit

---
+++ button散开/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  button散开
+//
+//  Created by apple on 16/01/16.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ button散开/ViewController.m
@@ -0,0 +1,80 @@
+//
+//  ViewController.m
+//  button散开
+//
+//  Created by apple on 16/01/16.
+//  Copyright © 2016年 apple. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "DisperseBotton.h"
+
+@interface ViewController ()
+@property(nonatomic ,weak) UILabel *lab;
+@property (weak, nonatomic) DisperseBotton *disView;
+@end
+
+@implementation ViewController
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    //散开的view
+    DisperseBotton *disView = [[DisperseBotton alloc]init];
+    disView.frame = CGRectMake(100, 100, 50, 50);
+    disView.borderRect = self.view.frame;
+    disView.closeImage = [UIImage imageNamed:@"icon2"];
+    disView.openImage = [UIImage imageNamed:@"icon3"];
+    
+    [self.view addSubview:disView];
+    _disView = disView;
+    [self setDisViewButtonsNum:3];
+    
+    //调试使用
+    UILabel *lab = [UILabel new];
+    lab.bounds = CGRectMake(0, 0, 300, 200);
+    lab.font = [UIFont systemFontOfSize:30];
+    lab.textAlignment = NSTextAlignmentCenter;
+    lab.center = self.view.center;
+    _lab = lab;
+    [self.view addSubview:lab];
+    
+}
+
+//调试使用
+- (IBAction)sliderTagget:(UISlider *)sender {
+    
+    int num = sender.value;
+    NSLog(@"%d",num);
+    [self setDisViewButtonsNum:num];
+    
+}
+
+- (void)setDisViewButtonsNum:(int)num{
+    
+    [_disView recoverBotton];
+    
+    for (UIView *btn in _disView.btns) {
+        [btn removeFromSuperview];
+    }
+    
+    NSMutableArray *marr = [NSMutableArray array];
+    for (int i = 0; i< num; i++) {
+        UIButton *btn = [UIButton new];
+        NSString *name = [NSString stringWithFormat:@"SC%d",i];
+        [btn setBackgroundImage:[UIImage imageNamed:name] forState:UIControlStateNormal];
+        [marr addObject:btn];
+        btn.tag = i;
+        [btn addTarget:self action:@selector(buttonTagget:) forControlEvents:UIControlEventTouchUpInside];
+    }
+    _disView.btns = [marr copy];
+}
+
+-(void)buttonTagget:(UIButton *)sender{
+    self.lab.text = [NSString stringWithFormat:@"点击了第%ld个按钮",sender.tag+1];
+}
+
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    self.lab.text = @"点击屏幕";
+}
+
+@end
